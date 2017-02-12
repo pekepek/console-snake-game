e8e0e5720666bc14e8b2fbc5c29288910d89e7bf
pekepek <ishihata@33i.co.jp>
2017-02-12 16:56:46 +0900

    just just just move this []

--- console-snake-game.gemspec
+++ console-snake-game.gemspec
@@ -8,9 +8,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["pekepek"]
   spec.email         = ["ishihata@33i.co.jp"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{You can play the snake game in terminal}
+  spec.description   = %q{You can play the snake game in terminal}
+  spec.homepage      = "https://github.com/pekepek/console-snake-game"
 
   spec.files         = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_dependency "tty"
 end
--- lib/console_snake.rb
+++ lib/console_snake.rb
@@ -1,4 +1,76 @@
-require "console_snake/version"
+require 'tty'
+require 'console_snake/version'
 
-module ConsoleSnake
+class ConsoleSnake
+  class << self
+    def start
+      @direction = 'C'
+
+      tate, yoko = IO.console.winsize.map {|i| i + 1 }
+      @position = {x: yoko / 2, y: tate / 2}
+
+      Thread.new do
+        move_cursor
+      end
+
+      loop do
+        print "\e[2J"
+        print "\e[#{@position[:y]};#{@position[:x]}H"
+
+        case @direction
+        when 'A'
+          @position[:y] -= 1
+        when 'B'
+          @position[:y] += 1
+        when 'C'
+          @position[:x] += 2
+        when 'D'
+          @position[:x] -= 2
+        end
+
+        if /A|B/ === @direction
+          exit if @position[:y] < 0 || tate < @position[:y]
+        else
+          exit if @position[:x] < 0 || yoko < @position[:x]
+        end
+
+        print '[]'
+
+        sleep 0.05
+      end
+    end
+
+    private
+
+    def move_cursor
+      while key = STDIN.getch
+        # NOTE enable to cancel a game by ctrl-c
+        exit if key == "\C-c"
+
+        if key == "\e" && STDIN.getch == "["
+          key = STDIN.getch
+
+          next if /A|B/ === @direction && /A|B/ === key
+          next if /C|D/ === @direction && /C|D/ === key
+
+          @direction = key
+        end
+      end
+    end
+
+    def print_block
+      piece = '[]'
+
+      if /C|D/ === @direction
+        print piece * 4
+        print "\e[8D"
+      else
+        4.times do
+          print piece
+          print "\e[2D"
+          print "\e[B"
+        end
+      end
+    end
+  end
 end
