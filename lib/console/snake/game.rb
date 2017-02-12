cf6cbc84250bb4b01e7336c904110559c6f8ffca
pekepek <ishihata@33i.co.jp>
2017-02-12 16:33:30 +0900

    bundle gem console-snake-game

---
+++ lib/console/snake/game.rb
@@ -0,0 +1,9 @@
+require "console/snake/game/version"
+
+module Console
+  module Snake
+    module Game
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/console/snake/game/version.rb
@@ -0,0 +1,7 @@
+module Console
+  module Snake
+    module Game
+      VERSION = "0.1.0"
+    end
+  end
+end
